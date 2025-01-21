9614412f49eec18e4070c0fd5659a40c923b75c1
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2025-01-21 18:12:10 -0500

    finish task 1 and 2

---
+++ Wekk-2/InClass/js/2_dom.js
@@ -0,0 +1,14 @@
+window.onload = setup
+function setup() {
+    // console.log(document.querySelector("#one").textContent)
+    //get the group
+    // let allSquareShapes = document.querySelectorAll(".square_shape");
+    // //go through each element
+    // for (let singleSquareShape of allSquareShapes) {
+    //     //get children
+    //     console.log(singleSquareShape.children[0])
+    //     singleSquareShape.children[0].textContent += "adding content"
+    // }
+    //document.querySelector(".square_shape").classList.remove("square_shape"); //first one only
+    document.querySelector("p span").classList.add("change_span");
+}
