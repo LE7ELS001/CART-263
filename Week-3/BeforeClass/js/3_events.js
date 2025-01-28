69a8a1485c296a97579727babfdf2cf942b53e52
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2025-01-28 12:37:37 -0500

    Update 3_events.js

--- Week-3/BeforeClass/js/3_events.js
+++ Week-3/BeforeClass/js/3_events.js
@@ -8,9 +8,32 @@ console.log(introSection);
 introSection.addEventListener("click", function (e) {
     // console.log(this);
     // console.log(e);
-    console.log(document.querySelector(`#${this.id} p`));
+    //console.log(document.querySelector(`#${this.id} p`));
+    let cssObj = window.getComputedStyle(this, null);
+    let bgColor = cssObj.getPropertyValue("background-color");
+    console.log(bgColor);
+    getColorObj(bgColor);
     document.querySelector(`#${this.id} p`).style.background = `rgba(125, 239, 110, 0.75)`;
-
+    console.log(this);
+    this.style.setProperty("opacity", ".5");
     //a:
     this.style.background = `rgba(214, 110, 239, 0.5)`
+
+
 });
+
+function getColorObj(color) {
+    let colorArray = color.match(/(\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})(?:,\s*(\d+(\.\d+)?))?/);
+    colorArray.splice(0, 1);
+    console.log(colorArray);
+
+    if (colorArray[3] && colorArray[4]) {
+        colorArray[3] = colorArray[3] + colorArray[4];
+        colorArray.splice(4, 1);
+    }
+
+    if (colorArray[4] === '.0') {
+
+    }
+    return colorArray;
+}
