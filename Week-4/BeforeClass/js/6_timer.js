c8eee4be719ea73b5285fc63ba54a7c8a2f7f2b9
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2025-02-04 12:15:54 -0500

    add before Class

---
+++ Week-4/BeforeClass/js/6_timer.js
@@ -0,0 +1,69 @@
+window.onload = function () {
+    let gridArray = [];
+    for (let i = 0; i < 24; i++) {
+        //for each x - make a column of changing y's
+        let gridCol = [];
+        for (let j = 0; j < 24; j++) {
+            //create a grid cell with a div
+            let parent = document.getElementById("parent");
+            let d = document.createElement("div");
+            d.classList.add("grid-cell");
+            parent.appendChild(d);
+
+            d.style.left = (i + 1) * 25 + "px";
+            d.style.top = (j + 1) * 25 + "px";
+            gridCol[j] = d;
+        }
+        //put each grid row into the grid array
+        gridArray[i] = gridCol;
+    }
+    console.log(gridArray);
+
+    let shades = [
+        "#7fb3d5", //grey blue first
+        "#76d7c4",
+        "#f7dc6f",
+        "#eb984e",
+        "#cb4335",
+        "#8e44ad",
+        "#2e4053",
+        "#e5e7e9",
+    ];
+
+    let num = 2;
+    setInterval(animate_cells_mod_rows, 1000);
+
+
+
+
+    function animate_cells_mod_rows() {
+        for (let i = 0; i < 24; i++) {
+            for (let j = 0; j < 24; j++) {
+                let d = gridArray[i][j]
+                //check the j--> y value for color choice (all same ys will have the same color (a row))
+                if (j % num === 0) {
+                    d.style.background = shades[0];
+                } else if (j % num === 1) {
+                    d.style.background = shades[1];
+                } else if (j % num === 2) {
+                    d.style.background = shades[2];
+                } else if (j % num === 3) {
+                    d.style.background = shades[3];
+                } else if (j % num === 4) {
+                    d.style.background = shades[4];
+                } else if (j % num === 5) {
+                    d.style.background = shades[5];
+                } else if (j % num === 6) {
+                    d.style.background = shades[6];
+                } else if (j % num === 7) {
+                    d.style.background = shades[7];
+                }
+            }
+        }
+        num += 1;
+        console.log(num);
+        if (num === 9) { num = 1 }
+    }
+
+
+}
---
+++ Week-4/BeforeClass/js/keyEx.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    // console.log("keys");
+
+    window.addEventListener("keydown", function (event) {
+        console.log(event);
+        //document.querySelector("#textContainer").textContent += event.key;
+        //document.querySelector("#textContainer").textContent += event.code;
+        let speedX = 5;
+        if (event.key === "ArrowRight") {
+
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) + speedX + "px";
+        }
+        else if (event.key === "ArrowLeft") {
+
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) - speedX + "px";
+        }
+        else if (event.code === "Space") {
+            let bool = document.getElementById("boxB").getAttribute("custom-bool");
+            if (bool === "off") {
+                document.getElementById("boxB").style.background = "orange";
+                document.getElementById("boxB").setAttribute("custom-bool", "on");
+            } else {
+                document.getElementById("boxB").style.background = "rgb(112, 184, 226)";
+                document.getElementById("boxB").setAttribute("custom-bool", "off");
+            }
+
+        }
+        else if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(226, 112, 177)";
+        }
+    })
+
+    window.addEventListener("keyup", function (event) {
+        console.log("keyup")
+        //2: change color when space is down
+        if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(112, 184, 226)";
+
+        }
+        else {
+            console.log(`key up not shift:`)
+            console.log(event)
+        }
+
+    })
+
+    // window.setTimeout(addTimeoutText, 2000);
+    // function addTimeoutText() {
+    //     let parent = document.getElementById("parent");
+    //     parent.innerHTML += "<p> NEW TEXT TO APPEAR </p>";
+    // }
+
+    window.setInterval(addTextRecur, 2000);
+
+    function addTextRecur() {
+        let parent = document.getElementById("parent");
+        parent.innerHTML += "<p> NEW TEXT TO APPEAR RECUR </p>";
+    }
+
+
+}
