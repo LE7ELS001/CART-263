bd7ded6a2d18c2fbef76007300886a8a386cc428
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2025-03-21 13:57:44 -0400

    Finish task 1 to 4

--- Week-9/Task/CART263-TASK-7-8/CART263-TASK-7-8/js/main.js
+++ Week-9/Task/CART263-TASK-7-8/CART263-TASK-7-8/js/main.js
@@ -1,24 +1,76 @@
 window.onload = async function () {
     console.log("task 7-8");
 
+
+    //task 1 
     let response = await fetch("data/iris.json");
     let dataContext = await response.json();
 
-    let propertiesArray = []
+    let contextArray = []
     Object.keys(dataContext).forEach(key => {
         let properties = Object.keys(dataContext[key]);
-        propertiesArray.push(properties);
+        contextArray.push(properties);
 
     });
 
-    let tmpArray = propertiesArray.reduce((Arr, value) => {
-        if (!Arr.includes(value)) {
-            Arr.push(value);
-        }
-        return Arr
-    }, []);
+    let propertiesNameArray = [];
 
-    console.log(tmpArray);
+    contextArray.forEach(Array => {
+        Array.forEach(name => {
+            if (!propertiesNameArray.includes(name)) {
+                propertiesNameArray.push(name);
+            }
+        })
+    });
+
+
+
+    let propertiesElement = document.getElementById("propertiesName");
+    propertiesElement.textContent = "Properties : " + propertiesNameArray.join(" - ");
+
+
+
+    //taske 2
+
+    let possibleColor = ["#5d3fd3", "#a73fd3", "#d33fb5", "#d35d3f", "#d3a73f"];
+
+    let irisesWithColors = dataContext.map(element => {
+        let randomColor = possibleColor[Math.floor(Math.random() * possibleColor.length)];
+
+        return { ...element, color: randomColor };
+    })
+
+
+
+    let colorElement = document.getElementById("colorExample");
+    colorElement.textContent = "Properties with color : " + Object.keys(irisesWithColors[0]).join(" - ");
+
+
+    //task 3    
+    let filteredIrises = irisesWithColors.filter(element => element.sepalWidth >= 4);
+    console.log(filteredIrises);
+
+    let filterElement = document.getElementById("sepalWidthExample");
+    filterElement.textContent = "Properties with sepalWidth >= 4 : " + "\n";
+    for (let i = 0; i < filteredIrises.length; i++) {
+        filterElement.textContent += JSON.stringify(filteredIrises[i]);
+    }
+
+    //task 4 
+
+    let sum;
+    dataContext.reduce((acc, element) => {
+        console.log(element.petalLength);
+        sum = acc + element.petalLength;
+        return sum;
+    }, 0);
+
+    let average = sum / dataContext.length;
+
+    let petalLengthElement = document.getElementById("petalLengthResult");
+    petalLengthElement.textContent = "Average petalLength : " + average;
+
+
 
 
 }
