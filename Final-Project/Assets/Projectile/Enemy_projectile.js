f1f02bd873e036100495c3d72cd7452c3af594af
Alibaba1555 <tianshunwus@gmail.com>
2025-04-12 12:26:21 -0400

    enemy_projectile.js/scene1.js fix

--- Final-Project/Assets/Projectile/Enemy_projectile.js
+++ Final-Project/Assets/Projectile/Enemy_projectile.js
@@ -1,4 +1,3 @@
-
 class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, key) {
         super(scene, x, y, key);
@@ -16,7 +15,6 @@ class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
         this.projectileHeight = 50;
         this.isUsed = false;
 
-
         switch (key) {
             case 'mushroomProjectile':
                 this.setSize(10, 10);
@@ -26,10 +24,7 @@ class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
                 break;
         }
 
-
         this.effectManager = new effectManager(this.scene);
-
-
     }
 
     preUpdate(time, delta) {
@@ -38,7 +33,7 @@ class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
         this.traveledDistance += this.body.deltaAbsX();
 
         if (this.isOutOfRange()) {
-            this.body.reset(0, 0)
+            this.body.reset(0, 0);
             this.activeProjectile(false);
             this.traveledDistance = 0;
         }
@@ -51,34 +46,30 @@ class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
 
         this.anims.stop();
         this.play(this.key, true);
-        // this.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
-        //     if (!this.isUsed) {
-        //         this.cleanupProjectile();
-        //     }
-        // });
         this.setVelocityX(this.speed);
+    }
+
+    tryHitPlayer(player) {
+        if (!this.isUsed && !player.invincible) {
+            player.takesHit(this);
+            this.cleanupProjectile();
+        }
     }
 
     deliverHit(target) {
         if (!this.isUsed) {
-
             this.cleanupProjectile();
         }
-        //this.effectManager.playEffectOn("enemyDead", target);
-
-
-
     }
 
     activeProjectile(isActive) {
         this.setActive(isActive);
         this.setVisible(isActive);
-
     }
 
     isOutOfRange() {
         return this.traveledDistance &&
-            this.traveledDistance >= this.maxDistance;
+               this.traveledDistance >= this.maxDistance;
     }
 
     cleanupProjectile() {
@@ -87,8 +78,100 @@ class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
         this.traveledDistance = 0;
         this.body.reset(0, 0);
     }
-
-
 }
 
 window.Enemy_projectile = Enemy_projectile;
+
+// class Enemy_projectile extends Phaser.Physics.Arcade.Sprite {
+//     constructor(scene, x, y, key) {
+//         super(scene, x, y, key);
+
+//         scene.add.existing(this);
+//         scene.physics.add.existing(this);
+
+//         this.speed = 250;
+//         this.maxDistance = 550;
+//         this.traveledDistance = 0;
+//         this.key = key;
+//         this.damage = 30;
+//         this.coolDown = 1000;
+//         this.projectileWdith = 50;
+//         this.projectileHeight = 50;
+//         this.isUsed = false;
+
+
+//         switch (key) {
+//             case 'mushroomProjectile':
+//                 this.setSize(10, 10);
+//                 this.setOffset(19, 21);
+//                 this.setScale(2.5);
+//                 this.setOrigin(0.5, 0.5);
+//                 break;
+//         }
+
+
+//         this.effectManager = new effectManager(this.scene);
+
+
+//     }
+
+//     preUpdate(time, delta) {
+//         super.preUpdate(time, delta);
+
+//         this.traveledDistance += this.body.deltaAbsX();
+
+//         if (this.isOutOfRange()) {
+//             this.body.reset(0, 0)
+//             this.activeProjectile(false);
+//             this.traveledDistance = 0;
+//         }
+//     }
+
+//     fire(x, y) {
+//         this.isUsed = false;
+//         this.activeProjectile(true);
+//         this.body.reset(x, y);
+
+//         this.anims.stop();
+//         this.play(this.key, true);
+//         // this.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+//         //     if (!this.isUsed) {
+//         //         this.cleanupProjectile();
+//         //     }
+//         // });
+//         this.setVelocityX(this.speed);
+//     }
+
+//     deliverHit(target) {
+//         if (!this.isUsed) {
+
+//             this.cleanupProjectile();
+//         }
+//         //this.effectManager.playEffectOn("enemyDead", target);
+
+
+
+//     }
+
+//     activeProjectile(isActive) {
+//         this.setActive(isActive);
+//         this.setVisible(isActive);
+
+//     }
+
+//     isOutOfRange() {
+//         return this.traveledDistance &&
+//             this.traveledDistance >= this.maxDistance;
+//     }
+
+//     cleanupProjectile() {
+//         this.isUsed = true;
+//         this.activeProjectile(false);
+//         this.traveledDistance = 0;
+//         this.body.reset(0, 0);
+//     }
+
+
+// }
+
+// window.Enemy_projectile = Enemy_projectile;
