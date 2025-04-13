5b2c42fa1ae84c11c2e99cee88d09252b602a29f
LE7ELS001 <95457016+LE7ELS001@users.noreply.github.com>
2025-04-13 18:27:56 -0400

    fix repeatly create player Animation

--- Final-Project/entities/anims/PlayerAnims.js
+++ Final-Project/entities/anims/PlayerAnims.js
@@ -1,104 +1,131 @@
 
 function PlayerAnimation(anims) {
-    anims.create({
-        key: "idle",
-        frames: anims.generateFrameNumbers("avatar-idle", {
-            start: 0,
-            end: 9
-        }),
-        frameRate: 8,
-        repeat: -1
-    });
+    if (!anims.exists('idle')) {
 
-    anims.create({
-        key: "run",
-        frames: anims.generateFrameNumbers("avatar-run", {
-            start: 0,
-            end: 9
-        }),
-        frameRate: 15,
-        repeat: -1
-    });
+        anims.create({
+            key: "idle",
+            frames: anims.generateFrameNumbers("avatar-idle", {
+                start: 0,
+                end: 9
+            }),
+            frameRate: 8,
+            repeat: -1
+        });
+    }
 
-    anims.create({
-        key: "attack1",
-        frames: anims.generateFrameNumbers("attack1", { start: 0, end: 3 }),
-        frameRate: 15,
-        repeat: 0
-    });
+    if (!anims.exists('run')) {
+        anims.create({
+            key: "run",
+            frames: anims.generateFrameNumbers("avatar-run", {
+                start: 0,
+                end: 9
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+    }
 
-    anims.create({
-        key: "attack2",
-        frames: anims.generateFrameNumbers("attack2", { start: 0, end: 5 }),
-        frameRate: 15,
-        repeat: 0
-    });
+    if (!anims.exists('attack1')) {
 
-    anims.create({
-        key: "roll",
-        frames: anims.generateFrameNumbers("roll", { start: 0, end: 11 }),
-        frameRate: 22,
-        repeat: 0
-    });
+        anims.create({
+            key: "attack1",
+            frames: anims.generateFrameNumbers("attack1", { start: 0, end: 3 }),
+            frameRate: 15,
+            repeat: 0
+        });
+    }
 
+    if (!anims.exists('attack1')) {
 
-    anims.create({
-        key: "jump",
-        frames: anims.generateFrameNumbers("avatar-jump", {
-            start: 0,
-            end: 2
-        }),
-        frameRate: 12,
-        repeat: -1
-    });
+        anims.create({
+            key: "attack2",
+            frames: anims.generateFrameNumbers("attack2", { start: 0, end: 5 }),
+            frameRate: 15,
+            repeat: 0
+        });
+    }
 
-    anims.create({
-        key: "fall",
-        frames: anims.generateFrameNumbers("avatar-fall", {
-            start: 0,
-            end: 2
-        }),
-        frameRate: 12,
-        repeat: -1,
+    if (!anims.exists('roll')) {
+
+        anims.create({
+            key: "roll",
+            frames: anims.generateFrameNumbers("roll", { start: 0, end: 11 }),
+            frameRate: 22,
+            repeat: 0
+        });
+    }
 
 
-    });
+    if (!anims.exists('jump')) {
+        anims.create({
+            key: "jump",
+            frames: anims.generateFrameNumbers("avatar-jump", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 12,
+            repeat: -1
+        });
+    }
+    if (!anims.exists('fall')) {
+        anims.create({
+            key: "fall",
+            frames: anims.generateFrameNumbers("avatar-fall", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 12,
+            repeat: -1,
 
-    anims.create({
-        key: "jumpfall",
-        frames: anims.generateFrameNumbers("avatar-Jumpfall", {
-            start: 0,
-            end: 1
-        }),
-        frameRate: 10,
-        repeat: 0,
-        //stopOnLastFrame: true
 
-    });
+        });
+    }
 
-    anims.create({
-        key: "wallslide",
-        frames: anims.generateFrameNumbers("avatar-wallslide", {
-            start: 0,
-            end: 2
-        }),
-        frameRate: 10,
-        repeat: -1
-    })
+    if (!anims.exists('jumpfall')) {
+        anims.create({
+            key: "jumpfall",
+            frames: anims.generateFrameNumbers("avatar-Jumpfall", {
+                start: 0,
+                end: 1
+            }),
+            frameRate: 10,
+            repeat: 0,
+            //stopOnLastFrame: true
 
-    anims.create({
-        key: "takesHit",
-        frames: [{ key: 'avatar-takesHit', frame: 0 }],
-        frameRate: 1,
-        repeat: -1
-    });
+        });
+    }
 
-    anims.create({
-        key: "launch",
-        frames: anims.generateFrameNumbers("attack1", { start: 0, end: 3 }),
-        frameRate: 15,
-        repeat: 0
-    });
+    if (!anims.exists('wallslide')) {
+
+        anims.create({
+            key: "wallslide",
+            frames: anims.generateFrameNumbers("avatar-wallslide", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+
+    if (!anims.exists('takesHit')) {
+
+        anims.create({
+            key: "takesHit",
+            frames: [{ key: 'avatar-takesHit', frame: 0 }],
+            frameRate: 1,
+            repeat: -1
+        });
+    }
+
+    if (!anims.exists('launch')) {
+        anims.create({
+            key: "launch",
+            frames: anims.generateFrameNumbers("attack1", { start: 0, end: 3 }),
+            frameRate: 15,
+            repeat: 0
+        });
+    }
 }
 
 
